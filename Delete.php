2b1cf6aff5ecb5f50c97624a880fc3a3c15fae1c
Shahzaib <shahzaibux@gmail.com>
2020-06-13 00:36:22 +0500

    Updated Delete Page

---
+++ Delete.php
@@ -0,0 +1,21 @@
+<?php
+
+    $con = mysqli_connect('localhost:3308', 'root', '', 'companydb');
+    $id = $_GET['id'];
+    $delete_query = "delete FROM employee WHERE EmpId = $id";
+    $execute = mysqli_query($con, $delete_query);
+
+    if ($execute == true) {
+        echo "<script>
+                alert('Data Delete Successfully');
+                window.location.href = 'Read.php';
+              </script>"
+    } else {
+        echo "<script>
+                alert('Data Not Delete');
+                window.location.href = 'Read.php';
+              </script>"
+    }
+    
+
+?>
